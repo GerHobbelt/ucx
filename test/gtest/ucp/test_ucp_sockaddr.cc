6faa9bec20c98d05d5e0935d779319331f3182be
Evgeny Leksikov <evgenylek@mellanox.com>
2021-08-26 18:24:44 +0300

    GTEST/UCP/TAG: test unexp queue cleanup

--- test/gtest/ucp/test_ucp_sockaddr.cc
+++ test/gtest/ucp/test_ucp_sockaddr.cc
@@ -55,7 +55,8 @@ public:
     enum {
         SEND_DIRECTION_C2S  = UCS_BIT(0), /* send data from client to server */
         SEND_DIRECTION_S2C  = UCS_BIT(1), /* send data from server to client */
-        SEND_DIRECTION_BIDI = SEND_DIRECTION_C2S | SEND_DIRECTION_S2C /* bidirectional send */
+        SEND_DIRECTION_BIDI = SEND_DIRECTION_C2S | SEND_DIRECTION_S2C, /* bidirectional send */
+        SEND_NO_RECV        = UCS_BIT(2)  /* do not recv data to test unexp Q cleanup */
     };
 
     typedef enum {
@@ -467,7 +468,7 @@ public:
     void send_recv(entity& from, entity& to, send_recv_type_t send_recv_type,
                    bool wakeup,
                    ucp_test_base::entity::listen_cb_type_t cb_type,
-                   size_t ep_index = 0)
+                   bool no_recv = false, size_t ep_index = 0)
     {
         const uint64_t send_data = ucs_generate_uuid(0);
         ucs_status_t send_status;
@@ -477,15 +478,20 @@ public:
 
         uint64_t recv_data = 0;
         void *recv_req;
-        if (send_recv_type == SEND_RECV_TAG) {
-            recv_req = recv(to, &recv_data, sizeof(recv_data),
-                            rtag_complete_cbx, NULL);
-        } else if (send_recv_type == SEND_RECV_STREAM) {
-            recv_req = recv(to, &recv_data, sizeof(recv_data),
-                            rstream_complete_cbx, NULL);
+
+        if (!no_recv) {
+            if (send_recv_type == SEND_RECV_TAG) {
+                recv_req = recv(to, &recv_data, sizeof(recv_data),
+                                rtag_complete_cbx, NULL);
+            } else if (send_recv_type == SEND_RECV_STREAM) {
+                recv_req = recv(to, &recv_data, sizeof(recv_data),
+                                rstream_complete_cbx, NULL);
+            } else {
+                UCS_TEST_ABORT("unsupported communication type " +
+                               std::to_string(send_recv_type));
+            }
         } else {
-            UCS_TEST_ABORT("unsupported communication type " +
-                           std::to_string(send_recv_type));
+            UCS_TEST_MESSAGE << "Do not recv";
         }
 
         {
@@ -497,8 +503,10 @@ public:
             }
         }
 
-        request_wait(recv_req, 0, wakeup);
-        EXPECT_EQ(send_data, recv_data);
+        if (!no_recv) {
+            request_wait(recv_req, 0, wakeup);
+            EXPECT_EQ(send_data, recv_data);
+        }
     }
 
     bool wait_for_server_ep(bool wakeup)
@@ -576,12 +584,12 @@ public:
 
         if (flags & SEND_DIRECTION_C2S) {
             send_recv(sender(), receiver(), send_recv_type(), wakeup,
-                      cb_type());
+                      cb_type(), flags & SEND_NO_RECV);
         }
 
         if (flags & SEND_DIRECTION_S2C) {
             send_recv(receiver(), sender(), send_recv_type(), wakeup,
-                      cb_type());
+                      cb_type(), flags & SEND_NO_RECV);
         }
     }
 
@@ -906,6 +914,10 @@ UCS_TEST_P(test_ucp_sockaddr, listen_s2c) {
 
 UCS_TEST_P(test_ucp_sockaddr, listen_bidi) {
     listen_and_communicate(false, SEND_DIRECTION_BIDI);
+}
+
+UCS_TEST_P(test_ucp_sockaddr, listen_bidi_no_recv) {
+    listen_and_communicate(false, SEND_DIRECTION_BIDI | SEND_NO_RECV);
 }
 
 UCS_TEST_P(test_ucp_sockaddr, ep_query) {
@@ -2672,6 +2684,10 @@ protected:
 
     void test_tag_send_recv(size_t size, bool is_exp,
                             bool is_sync = false) {
+        if (!is_exp) {
+            UCS_TEST_SKIP_R("ucp_tag_probe_nb + err handling is not supported");
+        }
+
         /* warmup */
         test_ucp_sockaddr_protocols::test_tag_send_recv(size, is_exp, is_sync);
 
@@ -2799,7 +2815,7 @@ protected:
 
             /* Warmup */
             send_recv(sender(), receiver(), send_recv_type(), false, cb_type(),
-                      sender_idx);
+                      false, sender_idx);
 
             void *sreq = send(sender(), send_buf.ptr(), size, SEND_RECV_TAG,
                               send_cb, NULL, sender_idx);
